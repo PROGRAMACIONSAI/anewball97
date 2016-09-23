290ff9f4327ba3f0acc2cc9985d141fabed3274b
anewball97 <anewball97@hotmail.com>
2016-09-22 19:38:21 -0500

    Sitio Ver1
    Sitio Html

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+var btnMenu = docuemnt.getElementsById('btn-menu');
+var nav = docuemnt.getElementsById('nav');
+
+btnMenu.addEventListener('click', function(){
+nav.classlist.toggle('mostrar');
+})
+
+
